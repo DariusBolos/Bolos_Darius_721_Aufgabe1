4bf538186c60276649ef053aef6238d4ae7bfd25
Darius Bolos <63288528+DariusBolos@users.noreply.github.com>
2025-01-15 12:17:41 +0200

    1a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,81 @@
 package org.example;
 
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(System.getProperty("user.dir") + "/data/" + "evenimente.json")) {
+            MyData[] myData = gson.fromJson(reader, MyData[].class);
+
+            for(MyData data: myData) {
+                System.out.println(data.Mitgliedsname);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class MyData {
+        private int Id;
+        private String Mitgliedsname;
+        private String Haus;
+        private String Ereignis;
+        private String Datum;
+
+
+        public int getId() {
+            return Id;
+        }
+
+        public void setId(int id) {
+            Id = id;
+        }
+
+        public String getMitgliedsname() {
+            return Mitgliedsname;
+        }
+
+        public void setMitgliedsname(String mitgliedsname) {
+            Mitgliedsname = mitgliedsname;
+        }
+
+        public String getHaus() {
+            return Haus;
+        }
+
+        public void setHaus(String haus) {
+            Haus = haus;
+        }
+
+        public String getEreignis() {
+            return Ereignis;
+        }
+
+        public void setEreignis(String ereignis) {
+            Ereignis = ereignis;
+        }
+
+        public String getDatum() {
+            return Datum;
+        }
+
+        public void setDatum(String datum) {
+            Datum = datum;
+        }
+
+        @Override
+        public String toString() {
+            return "MyData{" +
+                    "Id=" + Id +
+                    ", Mitgliedsname='" + Mitgliedsname + '\'' +
+                    ", Haus='" + Haus + '\'' +
+                    ", Ereignis='" + Ereignis + '\'' +
+                    ", Datum='" + Datum + '\'' +
+                    '}';
+        }
     }
 }
