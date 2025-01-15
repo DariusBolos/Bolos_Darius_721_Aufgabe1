994b6117ef52b8e564486b1ed366ff8b11bda713
Darius Bolos <63288528+DariusBolos@users.noreply.github.com>
2025-01-15 12:53:58 +0200

    1d

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,11 @@ package org.example;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.text.DateFormat;
 import java.util.*;
+import java.time.LocalDate;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,6 +20,11 @@ public class Main {
         }
 
         printDataWithInitial(data);
+
+        //printSortedStarkStudents(data);
+
+
+        writeHouseResultToFile(data, "results.txt");
     }
 
     public static List<MyData> getFromJsonFile() {
@@ -42,12 +51,90 @@ public class Main {
                 .forEach(d-> System.out.println(d.getMitgliedsname()));
     }
 
+    public static void printSortedStarkStudents(List<MyData> data) {
+        List<MyData> filteredData = data
+                .stream()
+                .filter(s-> s.Haus == Haus.Stark)
+                .toList();
+
+        List<String> names = new ArrayList<>();
+
+        for (MyData data1 : filteredData) {
+            names.add(data1.getMitgliedsname());
+        }
+
+        names
+                .stream()
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public static Result getResultForHouse(Haus house, List<MyData> data) {
+        List<MyData> filteredData = data
+                .stream()
+                .filter(s -> s.getHaus() == house)
+                .toList();
+
+        int points = filteredData.size();
+
+        String houseName = house.name();
+
+        return new Result(houseName, points);
+    }
+
+    public static void writeHouseResultToFile(List<MyData> data, String filename) {
+        List<Result> results = getOrderedHouses(data);
+
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + "/data/" + filename));
+            for (Result result : results) {
+                String line = result.getName() + '#' + result.getScore();
+                writer.write(line);
+                writer.newLine();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static List<Result> getOrderedHouses(List<MyData> students) {
+        List<Result> results = new ArrayList<>();
+        for (Haus house : Haus.values()) {
+            results.add(getResultForHouse(house, students));
+        }
+
+        return results
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .toList();
+    }
+
+    public static enum Haus {
+        Stark,
+        Lannister,
+        Targaryen,
+        Baratheon,
+        GreyJoy,
+        Martell,
+        Tyrell,
+    }
+
     public static class MyData {
         private int Id;
         private String Mitgliedsname;
-        private String Haus;
+        private Haus Haus;
         private String Ereignis;
-        private String Datum;
+        private Date Datum;
 
 
         public int getId() {
@@ -66,12 +153,12 @@ public class Main {
             Mitgliedsname = mitgliedsname;
         }
 
-        public String getHaus() {
-            return Haus;
+        public Haus getHaus() {
+            return this.Haus;
         }
 
-        public void setHaus(String haus) {
-            Haus = haus;
+        public void setHaus(Haus haus) {
+            this.Haus = haus;
         }
 
         public String getEreignis() {
@@ -82,12 +169,12 @@ public class Main {
             Ereignis = ereignis;
         }
 
-        public String getDatum() {
+        public Date getDatum() {
             return Datum;
         }
 
-        public void setDatum(String datum) {
-            Datum = datum;
+        public void setDatum(Date datum) {
+            this.Datum = datum;
         }
 
         @Override
@@ -110,6 +197,57 @@ public class Main {
                     ", Haus='" + Haus + '\'' +
                     ", Ereignis='" + Ereignis + '\'' +
                     ", Datum='" + Datum + '\'' +
+                    '}';
+        }
+    }
+
+    public static class Result implements Comparable<Result> {
+        private String name;
+        private int score;
+
+        public Result(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Result o) {
+            return Integer.compare(this.score, o.score);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Result result = (Result) o;
+            return score == result.score;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(score);
+        }
+
+        @Override
+        public String toString() {
+            return "Result{" +
+                    "name='" + name + '\'' +
+                    ", score=" + score +
                     '}';
         }
     }
