1a3f9af54be43b3e90ebef36f5e4c046a9da64e1
Darius Bolos <63288528+DariusBolos@users.noreply.github.com>
2025-01-15 12:25:53 +0200

    1 b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,44 @@ package org.example;
 
 import com.google.gson.Gson;
 
+import java.util.*;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List <MyData> data = getFromJsonFile();
+        for (MyData entry: data) {
+            System.out.println(entry);
+        }
+
+        printDataWithInitial(data);
+    }
+
+    public static List<MyData> getFromJsonFile() {
         Gson gson = new Gson();
+        List<MyData> data = new ArrayList<>();
+
         try (FileReader reader = new FileReader(System.getProperty("user.dir") + "/data/" + "evenimente.json")) {
             MyData[] myData = gson.fromJson(reader, MyData[].class);
 
-            for(MyData data: myData) {
-                System.out.println(data.Mitgliedsname);
-            }
+            return Arrays.stream(myData).toList();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return data;
+    }
+
+    public static void printDataWithInitial(List<MyData> data){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter initial: ");
+        char initial = scanner.next().charAt(0);
+        data.stream().filter(s-> s.getMitgliedsname().charAt(0) == initial)
+                .distinct()
+                .forEach(d-> System.out.println(d.getMitgliedsname()));
     }
 
     public static class MyData {
@@ -65,6 +88,18 @@ public class Main {
 
         public void setDatum(String datum) {
             Datum = datum;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            MyData data = (MyData) o;
+            return Objects.equals(Mitgliedsname, data.Mitgliedsname);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(Mitgliedsname);
         }
 
         @Override
